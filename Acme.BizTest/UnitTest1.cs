b13c7699633cdf35d13fc265b85c8a8a622685e3
NavidFa <navid.fallahazad@gmail.com>
2017-01-25 16:52:02 -0700

    first commit

---
+++ Acme.BizTest/UnitTest1.cs
@@ -0,0 +1,15 @@
+using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using AcmeApp1;
+
+namespace Acme.BizTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
---
+++ AcmeApp1/Product.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AcmeApp1
+{/// <summary>
+/// 
+/// </summary>
+    public class Product
+    {
+        private string productNamw;
+
+        public string ProductName
+        {
+            get { return productNamw; }
+            set { productNamw = value; }
+        }
+        private string description;
+
+        public string Description
+        {
+            get { return description; }
+            set { description = value; }
+        }
+        private int productId;
+
+        public int ProductId
+        {
+            get { return productId; }
+            set { productId = value; }
+        }
+        public string SayHello()
+        {
+            return "Hello" + ProductName + "(" + ProductId + "):" + Description;
+        }
+
+    }
+}
